e543d152a60365dbd22e7cbb1d64e1b5bb983cdb
Chris B <chbema.dev@gmail.com>
2025-04-29 15:35:04 +0200

    Wording of About page & Hide homepage

--- coffee-blog/src/consts.ts
+++ coffee-blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Coffee Paradise';
+export const SITE_DESCRIPTION = 'Coffee Paradise recense les bonnes pratiques et réunit les amateurs autour d\'une passion : le café';
